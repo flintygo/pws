93fe8ebd0c8d4f71262989ea2d1180348aa5f3dd
Timmetie01 <143613032+Timmetie01@users.noreply.github.com>
2023-09-21 10:23:08 +0200

    Comments laser.cs

--- Assets/Scripts/Physics/Laser.cs
+++ Assets/Scripts/Physics/Laser.cs
@@ -22,22 +22,28 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Clear point list to not get duplicates
         points.Clear();
+
+        // Add starting point
         points.Add(new Vector3 (0, 0, 0));
 
         LineRenderer lineRenderer = GetComponent<LineRenderer>();
 
         RaycastHit hit;
 
+        // In which direction Raycast should go
         Vector3 LaserDirection = new Vector3 (1, 0, 0);
+
+        //do a While loop, condition !dead, check hit.transform.gameObject i tink i forgor
 
         if (Physics.Raycast(transform.position, LaserDirection, out hit, 20f, ~LaserEmitter))
         {
+            // Add (for now ending) point to laser point list
             points.Add(new Vector3 (0, 0, 1) * (hit.distance));
         }
         
-
+        // Render the points
         lineRenderer.SetPositions(points.ToArray());
     }
 }
