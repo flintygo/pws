75532a9a366c9785a5468dfb1bb8a71604297b0e
Timmetie01 <tnmoosterom@hotmail.com>
2023-10-05 14:02:52 +0200

    Door openspeed now is variable

--- Assets/scripts/Game-Behavior/doorTrigger.cs
+++ Assets/scripts/Game-Behavior/doorTrigger.cs
@@ -11,6 +11,7 @@ public class doorTrigger : MonoBehaviour
     public float xChange;
     public float yChange;
     public float zChange;
+    public float openingSpeed;
     private int collisionCount = 0;
     private Vector3 originalLocation;
 
@@ -32,7 +33,7 @@ public class doorTrigger : MonoBehaviour
     private void Update()
     {
         isOpened = (collisionCount > 0);
-        door.transform.position = Vector3.Lerp(door.transform.position, new Vector3(xChange, yChange, zChange) * (isOpened ? 1 : 0) + originalLocation, Time.deltaTime * 5f);
+        door.transform.position = Vector3.Lerp(door.transform.position, new Vector3(xChange, yChange, zChange) * (isOpened ? 1 : 0) + originalLocation, Time.deltaTime * openingSpeed);
 
         
     }
