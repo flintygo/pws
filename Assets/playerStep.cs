f1e5d4eb358fdfc66eccb4b3bfc550e6d9626c53
flintygo <flintbroer@gmail.com>
2023-10-05 15:05:47 +0200

    deleted the log things from playerstep

--- Assets/playerStep.cs
+++ Assets/playerStep.cs
@@ -21,12 +21,9 @@ public class playerStep : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         bump = other.bounds.size.y/2 + other.bounds.center.y - transform.position.y + 1f;
 
-        Debug.Log(bump);
-
         if (bump < 0.2f && !other.isTrigger){
             Vector3 temp = new Vector3(0,bump,0);
             transform.parent.transform.position += temp;
-            Debug.Log("Bump up");
         }
     }
 }
