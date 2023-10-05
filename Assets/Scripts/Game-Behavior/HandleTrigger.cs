e2c0faf315effa2fa4235c7986b92b2e3329e698
flintygo <flintbroer@gmail.com>
2023-10-05 15:51:46 +0200

    trigger ding gefixt.

--- Assets/Scripts/Game-Behavior/HandleTrigger.cs
+++ Assets/Scripts/Game-Behavior/HandleTrigger.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 public class HandleTrigger : MonoBehaviour
 {
     private bool open = false;
-    private bool previousTrigger = false;
+    private float triggerDelay = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
     
 
     // Update is called once per frame
     void Update()
     {
-        
+        triggerDelay -= 0.5f;
+        Debug.Log(open);
+    }
+
+    void FixedUpdate() {
+        if (triggerDelay > 0){
+            open = true;
+        }
+        else{
+            open = false;
+        }
+
+        //hier de line om die deur te openen van de andere trigger script
     }
 
     public void trigger(Color laserColor)
     {
         Debug.Log("Triggered!");
-        previousTrigger = true;
+        triggerDelay = 1f;
 
     }
 }
