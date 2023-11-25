72affb0203efaf18ae9df2b3be8bb3662d2c01db
Timmetie01 <tnmoosterom@hotmail.com>
2023-11-25 15:33:38 +0100

    level 2

--- Assets/scripts/Game-Behavior/doorTrigger.cs
+++ Assets/scripts/Game-Behavior/doorTrigger.cs
@@ -11,9 +11,15 @@ public class doorTrigger : MonoBehaviour
     public float xChange;
     public float yChange;
     public float zChange;
+    public float xRotationChange;
+    public float yRotationChange;
+    public float zRotationChange;
     public float openingSpeed;
     private int collisionCount = 0;
     private Vector3 originalLocation;
+    private float originalRotationX;
+    private float originalRotationY;
+    private float originalRotationZ;
 
 
     private void OnTriggerEnter(Collider other)
@@ -29,11 +35,16 @@ public class doorTrigger : MonoBehaviour
     private void Start()
     {
         originalLocation = door.transform.position;
+        originalRotationX = door.transform.rotation.x;
+        originalRotationY = door.transform.rotation.y;
+        originalRotationZ = door.transform.rotation.z;
     }
+
     private void Update()
     {
         isOpened = (collisionCount > 0);
         door.transform.position = Vector3.Lerp(door.transform.position, new Vector3(xChange, yChange, zChange) * (isOpened ? 1 : 0) + originalLocation, Time.deltaTime * openingSpeed);
+        transform.localRotation = Quaternion.Euler(originalRotationX,  originalRotationY + 180 * (isOpened ? 1 : 0), originalRotationZ);
 
         
     }
